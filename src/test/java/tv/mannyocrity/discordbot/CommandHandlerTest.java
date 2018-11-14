e728ec9b1682d7b9d35729d2852c195acac47b25
Marc <mannyocrity@gmail.com>
2018-11-14 15:28:10 -0500

    Added Travis file and first unit test to verify CI is working.
    Added junit to pom.

---
+++ src/test/java/tv/mannyocrity/discordbot/CommandHandlerTest.java
@@ -0,0 +1,13 @@
+package tv.mannyocrity.discordbot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CommandHandlerTest {
+
+    @Test
+    public void tempOne() {
+        assertTrue(true);
+    }
+}
