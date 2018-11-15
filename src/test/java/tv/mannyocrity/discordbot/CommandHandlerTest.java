be546fab38bf87c9360a56aae146036c04a62b18
Marc Maniscalco <mannyocrity@gmail.com>
2018-11-14 22:03:05 -0500

    Added a test to cause a build.

--- src/test/java/tv/mannyocrity/discordbot/CommandHandlerTest.java
+++ src/test/java/tv/mannyocrity/discordbot/CommandHandlerTest.java
@@ -2,6 +2,7 @@ package tv.mannyocrity.discordbot;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CommandHandlerTest {
@@ -9,5 +10,10 @@ public class CommandHandlerTest {
     @Test
     public void tempOne() {
         assertTrue(true);
+    }
+
+    @Test
+    public void tempTwo() {
+        assertFalse(false);
     }
 }
