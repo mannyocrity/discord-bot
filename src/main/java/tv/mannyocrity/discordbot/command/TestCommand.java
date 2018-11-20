e7bf4db1e9b4e98e5584933c469a1810065e93fc
Marc <mannyocrity@gmail.com>
2018-11-20 10:31:36 -0500

    Add logging.

--- src/main/java/tv/mannyocrity/discordbot/command/TestCommand.java
+++ src/main/java/tv/mannyocrity/discordbot/command/TestCommand.java
@@ -2,14 +2,17 @@ package tv.mannyocrity.discordbot.command;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import tv.mannyocrity.discordbot.BotUtils;
 import tv.mannyocrity.discordbot.Command;
 
+@Slf4j
 public class TestCommand implements Command {
 
     @Override
     public void runCommand(MessageReceivedEvent event, List<String> args) {
+        log.info("Executing testcommand command with args: {}", args);
         BotUtils.sendMessage(event.getChannel(), "You ran the test command with args: " + args);
     }
 }
